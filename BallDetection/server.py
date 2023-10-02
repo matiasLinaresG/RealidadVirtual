b9bb68d7f569e4307617a5b79c2ba3d0b6380cd0
juansibecas <juan.sibecas@gmail.com>
2023-10-02 10:16:49 -0300

    changed socket logic

--- BallDetection/server.py
+++ BallDetection/server.py
@@ -1,42 +1,50 @@
 import socket
 import cv2
 import balldetection2
-
+import time
 
 # Set the host and port for the server
 HOST = '127.0.0.1'
 PORT = 8080
 
 # Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the host and port
-server_socket.bind((HOST, PORT))
+# socket.bind((HOST, PORT))
 
 # Listen for incoming connections
-server_socket.listen()
+# socket.listen()
 
-print(f"Server listening on {HOST}:{PORT}")
+# print(f"Server listening on {HOST}:{PORT}")
 
 # Accept a new connection
-client_socket, client_address = server_socket.accept()
-print(f"Connected to {client_address}")
+# client_socket, client_address = server_socket.accept()
+# print(f"Connected to {client_address}")
+
+sock.connect((HOST, PORT))
 
 vs, lower, upper = balldetection2.camera_init()
 
 while True:
-    # Send data to Unity
-    data_to_send = "Hello Unity!"
-    client_socket.sendall(data_to_send.encode('utf-8'))
-
     # run ball detection algorithm
-    balldetection2.ball_detection(vs, lower, upper)
+    x, y, dist = balldetection2.ball_detection(vs, lower, upper, False)
+
+    # Send data to Unity
+    data_to_send = f"{x},{y},{dist}"
+    # data_to_send = "1,2,3"  # test
+    print(data_to_send)
+    sock.sendall(data_to_send.encode("utf-8"))
+    # client_socket.sendall(data_to_send.encode('utf-8'))
+
+    time.sleep(1.0)
 
     # if the 'q' key is pressed, stop the loop
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         balldetection2.end_camera(vs)
+        break
 
 
 # Close the connection
-client_socket.close()
+sock.close()
